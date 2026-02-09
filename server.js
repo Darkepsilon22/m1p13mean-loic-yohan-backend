3baa4d8a9bcd758795cb33321d85e9f55e64b5bc
Yohan <yohanrak61@gmail.com>
2026-02-09 09:20:02 +0300

    modif: ajout reservation temporaire du stock

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const { connectDB } = require('./src/config');
 const { startBoutiqueReservationCronJob } = require('./src/jobs/boutiqueReservationCron');
+const { startOrderExpirationJob } = require('./src/jobs/orderExpiration');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +14,7 @@ const startServer = async () => {
 
     // Start cron jobs
     startBoutiqueReservationCronJob();
+    startOrderExpirationJob();
 
     // Start Express server
     app.listen(PORT, () => {
